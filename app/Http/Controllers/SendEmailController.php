ad5f6ac3ec7ddf052816503ec35a8a76a5137f2c
mark-enriquez23 <markenriquez23.87@gmail.com>
2021-03-08 14:32:47 +0800

    added email sender

---
+++ app/Http/Controllers/SendEmailController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Mail\SendEmail;
+use Mail;
+
+class SendEmailController extends Controller
+{
+    
+    public function sendEmail(Request $request) {
+
+        $data = [
+            'fullName' => $request->data['fullName'], 
+            'userName' => $request->data['userName'],
+            'email' => $request->data['email']
+        ];
+
+        Mail::to($request->email)->send(new SendEmail($data));
+
+        return response()->json([
+            "success" => true,
+            "message" => __('succuess'),
+            "data" => $data
+        ]);
+    }
+
+}
---
+++ app/Mail/SendEmail.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class SendEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $data;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        $address = 'hanszerodii@gmail.com'; // Main SendGrid Account
+        $subject = 'New User!'; // Subject of the email
+        $name = $this->data['fullName']; // Name of the new user
+
+        return $this->view('emails.test')
+                    ->from($address, $name)
+                    ->cc($address, $name)
+                    ->bcc($address, $name)
+                    ->replyTo($address, $name)
+                    ->subject($subject)
+                    ->with([ 
+                        'fullName' => $this->data['fullName'],
+                        'userName' => $this->data['userName'],
+                        'email' => $this->data['email']
+                        ]);
+    }
+}
