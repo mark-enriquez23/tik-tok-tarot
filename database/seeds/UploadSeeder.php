1893e32756f0fbe5a0e496ee383879c0dff7ce00
mark-enriquez23 <markenriquez23.87@gmail.com>
2021-04-14 16:19:09 +0800

    aded new upload seeder

--- database/seeds/UploadSeeder.php
+++ database/seeds/UploadSeeder.php
@@ -27,7 +27,7 @@ class UploadSeeder extends Seeder
             [
                 'user_id'           => 2,
                 'name'              => "Test Stream/Live 2",
-                'content'           => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ac libero justo. Suspendisse",
+                'content'           => "Etiam at tortor arcu. Etiam maximus a mi ac pretium.",
                 'thumbnail'         => "test.jpg",
                 'src'               => "test.jpg",
                 'status'            => "streaming",
@@ -38,7 +38,7 @@ class UploadSeeder extends Seeder
             [
                 'user_id'           => 2,
                 'name'              => "Test Stream/Live 3",
-                'content'           => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ac libero justo. Suspendisse",
+                'content'           => "Donec eu ligula et erat elementum convallis sed vitae purus. Vivamus tincidunt neque ut felis pharetra ullamcorper. Maecenas efficitur nibh ac mollis imperdiet.",
                 'thumbnail'         => "test.jpg",
                 'src'               => "test.jpg",
                 'status'            => "streaming",
@@ -49,7 +49,7 @@ class UploadSeeder extends Seeder
             [
                 'user_id'           => 2,
                 'name'              => "Test Vlog 1",
-                'content'           => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ac libero justo. Suspendisse",
+                'content'           => "Integer molestie orci sit amet hendrerit ornare. Aenean suscipit nulla a tortor laoreet,",
                 'thumbnail'         => "test.jpg",
                 'src'               => "test.jpg",
                 'status'            => "ended",
@@ -60,13 +60,57 @@ class UploadSeeder extends Seeder
             [
                 'user_id'           => 2,
                 'name'              => "Test Vlog 2",
-                'content'           => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ac libero justo. Suspendisse",
+                'content'           => "Donec vulputate ipsum urna, vel laoreet nibh ornare quis",
                 'thumbnail'         => "test.jpg",
                 'src'               => "test.jpg",
                 'status'            => "ended",
                 'is_active'         => 1,
                 'is_featured'       => 1,
                 'upload_type_id'    => 2 // live
+            ],
+            [
+                'user_id'           => 2,
+                'name'              => "Test Vlog 3",
+                'content'           => "Aliquam sed scelerisque nisl. Vivamus ipsum lacus, efficitur ut hendrerit ut, dignissim rutrum nibh.",
+                'thumbnail'         => "test.jpg",
+                'src'               => "test.jpg",
+                'status'            => "ended",
+                'is_active'         => 1,
+                'is_featured'       => 1,
+                'upload_type_id'    => 2 // live
+            ],
+            [
+                'user_id'           => 2,
+                'name'              => "Test Vlog 4",
+                'content'           => "Donec et velit augue. Mauris dictum fringilla ex at pulvinar.",
+                'thumbnail'         => "test.jpg",
+                'src'               => "test.jpg",
+                'status'            => "ended",
+                'is_active'         => 1,
+                'is_featured'       => 1,
+                'upload_type_id'    => 2 // live
+            ],
+            [
+                'user_id'           => 2,
+                'name'              => "Test Stream/Live 4",
+                'content'           => "Convallis sit amet rhoncus quis, euismod posuere lacus. Proin malesuada ullamcorper sem, eget ",
+                'thumbnail'         => "test.jpg",
+                'src'               => "test.jpg",
+                'status'            => "streaming",
+                'is_active'         => 1,
+                'is_featured'       => 1,
+                'upload_type_id'    => 1 // live
+            ],
+            [
+                'user_id'           => 2,
+                'name'              => "Test Stream/Live 5",
+                'content'           => "sodales. Suspendisse et libero interdum, sagittis nisi vitae, aliquet ex",
+                'thumbnail'         => "test.jpg",
+                'src'               => "test.jpg",
+                'status'            => "streaming",
+                'is_active'         => 1,
+                'is_featured'       => 1,
+                'upload_type_id'    => 1 // live
             ],
         ];
         
